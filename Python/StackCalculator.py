3a981978df8e23dc5d6648500494887f4e6b72b7
Henry Chang <hchang23@g.ucla.edu>
2020-10-20 02:15:46 -0500

    Added additional ppt, more python practices

--- Python/StackCalculator.py
+++ Python/StackCalculator.py
@@ -1,3 +1,92 @@
-# Using a stack, implement a basic calulator that takes in posix equation 
+# Using a stack, implement a basic calulator that takes in Infix equation 
 # as an input and return the value. 
+
+#Convert infix notation into Postfix notation
+#Example: 
+# Infix eq: 1 + 2  
+# Postfix eq: 2 1 +
+
+def convertInfix(InfixExpr: str) -> str:
+
+    helper_stack = []
+    valid_operands = {'(' : 3, ')': 3, '^': 2, '*': 1, '/': 1, '+': 0, '-': 0}
+    result = ""
+    print(InfixExpr.split())
+    #loop through each element of the posfix expression
+    for key in InfixExpr.split():
+        
+        #check if key is a number 
+        if(key.isnumeric()):
+            result += key + ' '
+        
+        elif(key == '('):
+            helper_stack.append(key)
+
+        elif(key == ')'):
+            if(len(helper_stack) == 0):
+                print("The provided Infix equation is invalid")
+                return "Invalid EQ"
+            
+            else:
+                curr_key = helper_stack.pop()
+                while(curr_key != '('):
+                    result += curr_key + ' '
+                    
+                    curr_key = helper_stack.pop()
+        
+        #check if the key is a valid operand
+        elif(key in valid_operands):
+
+            if len(helper_stack) == 0:
+                helper_stack.append(key)
+            else:
+
+                #compare level of the operand. Append it to the result string
+                #until operand is ( or less than the current operation level
+                while len(helper_stack) != 0:
+                    curr_op = helper_stack[-1]
+
+                    if(valid_operands[curr_op] < valid_operands[key] or curr_op == '('):
+                        break
+                    else:
+                        result += helper_stack.pop() + ' '
+                helper_stack.append(key)
+                
+        else:
+            print("The provided Infix equation is invalid")
+            return "Invalid EQ"
+        
+        print(result)
+        print(helper_stack)
+                
+
+    #Get the remaining operators
+    while len(helper_stack) != 0:
+        result += helper_stack.pop() + ' '
+
+    return result
+
+print(convertInfix('( 1 + 2 ) * ( 2 + 3 )'))
+#Takes in the Infix expression than return the resulting
+#integer
+
+#Steps
+
+
+#Examples
+#Infix eq: 1 + 2 + 3 + 4 * 5
+#Postfix Notation: 1 2 + 3 + 4 5 * +
+
+#Infix eq: 1*2+3*4+5
+#Postfix: 1 2 * 3 4 * + 5 +
+
+#Time complexity: O(n)
+#Space complexity: O(n)
+# It is guaranteed that each item is separated by a space 
+def calculatePostfix(infixExpr: str) -> int:
+    return 0
+
+def calculateInfix(InfixExpr: str) -> int:
+    
+    return 0
 
--- Python/week5.py
+++ Python/week5.py
@@ -1,2 +1,90 @@
-x = [1,2,3,4,5,6,7]
-print(x[-1])
+class Child:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        print("I have set the value")
+    
+    def printInfo(self):
+        self.printName()
+        self.printAge()
+    
+    def printName(self):
+        print(self.name)
+    
+    def printAge(self):
+        print(self.age)
+    
+    def sayGibberish(self):
+        if(len(self.name) == 0):
+            return 
+        
+        print(self.name)
+        self.name = self.name[:-1]
+        self.sayGibberish()
+
+from collections import deque 
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+    
+    def serialize(self, root: TreeNode) -> str:
+        encoded_data = []
+
+        self.helpSerialize(root, encoded_data)
+        return ' '.join(map(str, encoded_data))
+    
+    def helpSerialize(self, root:TreeNode, encoded_data: list) -> None:
+        if(root is None):
+            return
+        else:
+            encoded_data.append(root.val)
+            self.helpSerialize(root.left, encoded_data)
+            self.helpSerialize(root.right, encoded_data)
+        return
+
+    def deserialize(self, data:str) -> TreeNode:
+        """
+        decodes data and reconstruct tree
+        """
+        # data = deque(map(int,data.strip(' ')))
+        
+        data = deque(map(int, data.split(' ')))
+        return self.helpDeserialize(data, -1, 100000)
+        
+    def helpDeserialize(self, data:deque, lowerbound, upperbound) -> TreeNode:
+        if(len(data) == 0):
+            return None
+        
+        if(lowerbound < data[0] < upperbound):
+            curr_val = data.popleft()
+            node = TreeNode(curr_val)
+            node.left = self.helpDeserialize(data, lowerbound, curr_val)
+            node.right = self.helpDeserialize(data, curr_val, upperbound)
+            return node
+        
+        
+
+root_node = TreeNode(1)
+left_node = TreeNode(0)
+right_node = TreeNode(2)
+
+root_node.left = left_node
+root_node.right = right_node
+
+def printTree(root: TreeNode) -> None:
+    if(root is None):
+        return
+    else:
+        printTree(root.left)
+        print(root.val)
+        printTree(root.right)
+
+testCodec = Codec()
+compressed_tree = testCodec.serialize(root_node)
+print(compressed_tree)
+printTree(testCodec.deserialize(compressed_tree))
